5087d23e9a19b74bb0a90905c3bf816aa72f7aee
Anne LoVerso <anne.loverso@gmail.com>
2020-09-26 17:36:36 -0400

    Updates

---
+++ precache-manifest.132cfc1b9766eb16dd64ffa7847f7c72.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97bd5923e98decde79a2d3c47973c33c",
+    "url": "/crowsmaille/index.html"
+  },
+  {
+    "revision": "4f142a62ce93e03749ee",
+    "url": "/crowsmaille/static/css/main.539a0afa.chunk.css"
+  },
+  {
+    "revision": "82b4bfdaa8ad8d428059",
+    "url": "/crowsmaille/static/js/2.b8f51b82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crowsmaille/static/js/2.b8f51b82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f142a62ce93e03749ee",
+    "url": "/crowsmaille/static/js/main.53a72ae3.chunk.js"
+  },
+  {
+    "revision": "80827e44800f34b9acdb",
+    "url": "/crowsmaille/static/js/runtime-main.7644e968.js"
+  },
+  {
+    "revision": "3bbb3da6b680a7bb0296d15c21a6dd3a",
+    "url": "/crowsmaille/static/media/chain_gold_red.3bbb3da6.jpg"
+  },
+  {
+    "revision": "6a9726f6799d04793d6ed4f1824ff526",
+    "url": "/crowsmaille/static/media/chain_rainbow.6a9726f6.jpg"
+  },
+  {
+    "revision": "d77b80502ca67c0857c78b71a7383b9c",
+    "url": "/crowsmaille/static/media/earring_opal.d77b8050.jpg"
+  },
+  {
+    "revision": "6af607015fc66aa46b6b869c9a00d195",
+    "url": "/crowsmaille/static/media/logo2.6af60701.svg"
+  }
+]);
